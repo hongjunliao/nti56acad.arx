db8425188e50daa1ba44d3e52b96e0799ef741b9
hongjun.liao <docici@126.com>
2020-08-25 19:00:51 +0800

    sorry, win32 + openGL dockin NOT working

--- src/main.cpp
+++ src/main.cpp
@@ -54,12 +54,9 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
         return true;
-    switch (msg)
-    {
-    case WM_SIZE:
-    {
-        if (wParam != SIZE_MINIMIZED)
-        {
+    switch (msg) {
+    case WM_SIZE:  {
+        if (wParam != SIZE_MINIMIZED)  {
             g_display_w = (UINT)LOWORD(lParam);
             g_display_h = (UINT)HIWORD(lParam);
         }
@@ -67,13 +64,10 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     }
     case WM_SYSCOMMAND:  {
         if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
-        {
             return 0;
-        }
         break;
     }
-    case WM_DESTROY:
-    {
+    case WM_DESTROY:  {
         PostQuitMessage(0);
         return 0;
     }
@@ -83,11 +77,13 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 bool Init(HINSTANCE hInstance)
 {
-    int style = (is_chld ? (WS_OVERLAPPEDWINDOW | WS_CHILD | WS_VISIBLE | WS_POPUP)
+    ImGui_ImplWin32_EnableDpiAwareness();
+
+    int wstyle = (is_chld ? (WS_OVERLAPPEDWINDOW | WS_CHILD | WS_VISIBLE | WS_POPUP)
                          : (WS_OVERLAPPEDWINDOW | WS_VISIBLE));
     static WNDCLASSEX wc = {sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("nti56acad"), NULL};
     ::RegisterClassEx(&wc);
-    g_hwnd = ::CreateWindowEx(WS_EX_TOPMOST, wc.lpszClassName, _T("nti56acad"), style, 100, 100, 200, 100, 0, NULL, wc.hInstance, NULL);
+    g_hwnd = ::CreateWindowEx(WS_EX_TOPMOST, wc.lpszClassName, _T("nti56acad"), wstyle, 100, 100, 200, 100, 0, NULL, wc.hInstance, NULL);
 
     // Show the window
     ShowWindow(g_hwnd, SW_SHOWDEFAULT);
@@ -100,8 +96,19 @@ bool Init(HINSTANCE hInstance)
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
-    //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
 
     //Init Win32
     ImGui_ImplWin32_Init(g_hwnd);
@@ -109,12 +116,9 @@ bool Init(HINSTANCE hInstance)
     //Init OpenGL Imgui Implementation
     // GL 3.0 + GLSL 130
     const char *glsl_version = "#version 130";
-    ImGui_ImplOpenGL3_Init(
-        glsl_version);
+    ImGui_ImplOpenGL3_Init(glsl_version);
     //Set Window bg color
     clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    // Setup style
-    ImGui::StyleColorsDark();
 
     return true;
 }
@@ -124,12 +128,11 @@ void Cleanup(HINSTANCE hInstance)
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
 
-    wglDeleteContext(
-        g_GLRenderContext);
+    wglDeleteContext(g_GLRenderContext);
+    ImGui_ImplWin32_Shutdown();
 
     ImGui::DestroyContext();
 
-    ImGui_ImplWin32_Shutdown();
     DestroyWindow(g_hwnd);
 
     UnregisterClass(L"IMGUI", hInstance);
@@ -138,6 +141,7 @@ void Cleanup(HINSTANCE hInstance)
 int wmain(int argc, wchar_t *argv[])
 {
     HINSTANCE hInstance = GetModuleHandle(NULL);
+    
     if (!Init(hInstance))
         return 1;
 
@@ -145,6 +149,7 @@ int wmain(int argc, wchar_t *argv[])
     // Main loop
     MSG msg;
     ZeroMemory(&msg, sizeof(msg));
+
     while (msg.message != WM_QUIT)  {
         // Poll and handle messages (inputs, window resize, etc.)
         // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
@@ -166,22 +171,23 @@ int wmain(int argc, wchar_t *argv[])
         // Rendering
         ImGui::Render();
 
-        wglMakeCurrent(g_HDCDeviceContext, g_GLRenderContext);
+		wglMakeCurrent( g_HDCDeviceContext,g_GLRenderContext);
 
-        glViewport(0, 0, g_display_w, g_display_h);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
+		glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		// Update and Render additional Platform Windows
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+		}
 
-        // Update and Render additional Platform Windows
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-        }
-        wglMakeCurrent(g_HDCDeviceContext, g_GLRenderContext);
-        SwapBuffers(g_HDCDeviceContext);
-    }
+        wglMakeCurrent( g_HDCDeviceContext, g_GLRenderContext);
+		SwapBuffers( g_HDCDeviceContext);
+	}
+
     Cleanup(hInstance);
     return 0;
 }
