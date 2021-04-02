d99c1afafa09cfb7ba5f976d144032e2dd341671
hongjun.liao <docici@126.com>
2021-04-02 15:03:46 +0800

    恢复测试工程nti56acad_test.vcxproj, 并进行了hp_iocp的测试

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,31 @@
 #endif /* HAVE_CONFIG_H */
 
 #include "stdafx.h"
+//#include <WinSock2.h>
+//#include <stdio.h>
+//#include <process.h>
+//#include <time.h>
+
+#include <locale.h>
+#include "libxhhp/hp_iocp.h"
+#include "libxhhp/hp_log.h"	 /* hp_log */
+#include "libxhhp/hp_test.h" /* hp_test */
+#include "libxhhp/hp_opt.h" /* hp_opt_argv */
+#include "libxhhp/hp_err.h"
+
+#define OPTPARSE_IMPLEMENTATION
+#define OPTPARSE_API static
+#include "optparse/optparse.h"		/* option */
+//#include "getopt.h"			/* getopt_long */
+
+#include <conio.h>			/* _getch() */
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "redis/src/zmalloc.h"
+#ifdef __cplusplus
+}
+#endif
 
 /////////////////////////////////////////////////////////////////////////////////////
 
@@ -24,10 +49,98 @@ int test_nti_imgui_main(int, char**);
 int win32_docking_gl3(int, char**);
 int win32_dx9_main(int, char**);
 
+static void outOfMemoryHandler(size_t allocation_size) {}
+
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPTSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
-	return win32_dx9_main(0, 0);
+	int i, rc;
+	setlocale(LC_COLLATE, "");
+	zmalloc_enable_thread_safeness();
+	zmalloc_set_oom_handler(outOfMemoryHandler);
+	srand((unsigned int)time(NULL) ^ getpid());                                   WIN_PORT_FIX /* cast (unsigned int) */
+
+	/* init COM */
+	HRESULT Hr = CoInitialize(NULL);
+	if (FAILED(Hr)) return 0;
+
+	/* init winsock */
+	WSADATA wsd;
+	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0) {
+		fprintf(stdout, "%s: error WSAStartup, err=%d\n", __FUNCTION__, WSAGetLastError());
+		return 1;
+	}
+
+	/* alloc console for debug if needed */
+#ifndef NDEBUG
+	//AllocConsole();
+	//FILE * fp = NULL;
+	//freopen_s(&fp, "CONOUT$", "w+t", stdout);
+	//     freopen_s(&fp, "CONOUT$", "w+t", stderr);
+	//     fprintf(stdout, "%s: cmdline='%s'\n", __FUNCTION__, lpCmdLine);
+#endif /* NDEBUG */
+
+	hp_log(stdout, "%s: build at %s %s\n", __FUNCTION__, __DATE__, __TIME__);
+
+	/* init argc,argv, as WinMain haven't provide them */
+	char cmdline[512] = "";
+	cmdline[0] = '\0';
+	strncpy(cmdline, GetCommandLineA(), sizeof(cmdline));
+#ifndef NDEBUG
+	/* sample: 'foo -h172.168.1.171 -p9200' */
+	fprintf(stdout, "%s: cmdline=%d/'%s'\n", __FUNCTION__, (int)strlen(cmdline), cmdline);
+#endif /* NDEBUG */
+
+	int argc = 0;
+	char * argv[512] = { 0 };
+	argc = hp_opt_argv(cmdline, argv);
+#ifndef NDEBUG
+	fprintf(stdout, "%s: argc=%d, argv=%p\n", __FUNCTION__, argc, argv);
+	for (i = 0; i < argc; ++i)
+		fprintf(stdout, "%s: argv[%d]=%s\n", __FUNCTION__, i, argv[i]);
+#endif /* NDEBUG */
+
+	/* run test? */
+	sds test = 0;
+
+	/* parse argc/argv */
+	struct optparse_long longopts[] = {
+		{"test", 't', OPTPARSE_REQUIRED},
+		{"help", 'h', OPTPARSE_NONE},
+		{0}
+	};
+	struct optparse options;
+	optparse_init(&options, argv);
+
+    for (; (rc= optparse_long(&options, longopts, NULL)) != -1; ) {
+        switch (rc) {
+        case 't':
+			test = sdsnew(options.optarg ? options.optarg : "");
+            break;
+		case 'h':
+		case '?':
+			fprintf(stdout, "test for nti56acad.arx\n"
+				"Usage: %s --test=testname[,testname2]\n"
+				, argv[0]);
+			break;
+		}
+	}
+
+	if(test)
+		rc = hp_test(test, argc, argv, 0, 1);
+	//test_hp_err_main(argc, argv);
+	//hp_iocp_test_main(argc, argv);
+
+	sdsfree(test);
+
+	/* clean */
+	WSACleanup();
+	::CoUninitialize();
+
+	fprintf(stdout, "%s: exited, Press ANY key to close\n", __FUNCTION__);
+	_getch();
+	return rc;
+	//return win32_dx9_main(0, 0);
 }
